dead7908e58a1b205501714eb7b1cb290acf34e6
Sarvesh Sawant <mailsarveshsawant@gmail.com>
2023-06-04 00:52:24 +0530

    Update content in About and Interests

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
     return (
         <div className="about">
             <h2 className="about--title">About</h2>
-            <p className="about--content">&Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto unde in officia sequi harum cum deleniti ipsa! Recusandae, alias enim veritatis ad odit nobis fugiat expedita possimus tenetur. In, unde.</p>
+            <p className="about--content">I am a frontend developer with a particular interest in making things simple. I try to keep up with the best practices, and am always looking for new things to learn.</p>
         </div>
     )
 }
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -3,7 +3,7 @@ export default function Interests() {
     return (
         <div className="interests">
             <h2 className="interests--title">Interests</h2>
-            <p className="interests--content">&Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores temporibus error magni excepturi fuga officia repellat. Culpa, tempora vel! Obcaecati aliquid repellat illo asperiores harum id culpa, nisi amet aut!</p>
+            <p className="interests--content">YouTube Fanatic. Podcast enthusiast.</p>
         </div>
     )
 }
